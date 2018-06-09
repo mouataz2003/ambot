57515e698084e95ea82900c52e3d34e3912a12ab
mouataz2003 <mouataz_bouazaoui@outlook.com>
2018-06-09 09:12:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const { prefix } = require('./config.json')
 
 bot.on('ready', () => {
     console.log(`[Start] ${new Date()}`);
+    
+  
+});
+
+music(bot, {
+    prefix: '-',       // Prefix of '-'.
+    global: false,     // Server-specific queues.
+    maxQueueSize: 10,  // Maximum queue size of 10.
+    clearInvoker: true, // If permissions applicable, allow the bot to delete the messages that invoke it (start with prefix)
+    channel: 'music'   // Name of voice channel to join. If omitted, will instead join user's voice channel.
 });
 
 
